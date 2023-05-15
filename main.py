6439aaa2c9d82d43648780bd40c5d4f1c1468e01
adrilava <99504457+adrilava@users.noreply.github.com>
2023-05-15 09:52:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,26 @@ TILE_SIZE = ((SCREEN_WIDTH / 2) / MAP_SIZE) - 7
 MAX_DEPTH = 750
 FOV = math.pi / 3
 HALF_FOV = FOV / 2
-CASTED_RAYS = 480
+CASTED_RAYS = 240
 STEP_ANGLE = FOV / CASTED_RAYS
 SCALE = (SCREEN_WIDTH) / CASTED_RAYS
 uncalc = 6
-wallHeight = 24000
+wallHeight = 28000
 qualityBrut = 10
 refList = [1, 3, 9]
 debug = False
-showMap = False
-screenPatch = -180
+showMap = True
+screenPatch = -120
+maxSpeed = 100
+
 
 
 
 player_x = (SCREEN_WIDTH / 2) / 2.5
-player_y = (SCREEN_WIDTH / 2) / 2.5
-player_angle = math.pi
+player_y = (SCREEN_WIDTH / 2) / 2.2
+player_angle = -math.pi/2
 
+divideSpeedMax = maxSpeed / 100
 absoluteMapSize = MAP_SIZE*TILE_SIZE
 
 MAP = (
@@ -48,7 +51,7 @@ MAP = (
     '           #      #           '
     ' ###### ## #      # ## ###### '
     '####### ## ######## ## #######'
-    '####### ##          ## #######'
+    '####### ##     +    ## #######'
     '####### ## ######## ## #######'
     '####### ## ######## ## #######'
     '##            ##            ##'
@@ -70,6 +73,40 @@ clock = pygame.time.Clock()
 
 
 
+def move(dir, MAP):
+
+
+
+    index = MAP.index("+")
+
+
+    if dir == 0 : newPos = index - 30
+    if dir == 1 : newPos = index + 1
+    if dir == 2 : newPos = index + 30
+    if dir == 3 : newPos = index - 1
+
+
+
+
+
+
+
+    if MAP[newPos] == " ":
+
+
+
+        MAP = MAP[:index] + " " + MAP[index+1:]
+
+        MAP = MAP[:newPos] + "+" + MAP[newPos+1:]
+
+
+
+
+    return MAP
+
+
+
+
 def draw_map():
     # MAP
     if showMap:
@@ -77,20 +114,26 @@ def draw_map():
             for col in range(MAP_SIZE):
                 square = row * MAP_SIZE + col
 
+                if MAP[square] == "^" : color = (200, 0, 102)
+                elif MAP[square] == "+" : color = (50, 50, 50)
+                elif MAP[square] == "#" : color = (0, 0, 102)
+                else : color = (0, 0, 0)
+
 
                 pygame.draw.rect(
                     win,
-                    (0, 0, 102) if MAP[square] == '#' else (0, 0, 0),
+                    color,
                                  ((col * TILE_SIZE) + (SCREEN_WIDTH - (MAP_SIZE * TILE_SIZE)),
                                  (row * TILE_SIZE) + SCREEN_HEIGHT - (MAP_SIZE * TILE_SIZE), TILE_SIZE , TILE_SIZE ),
                                  )
 
                 pygame.draw.circle(win, (255, 211, 0), (
-        int(player_x) + (SCREEN_WIDTH - (MAP_SIZE * TILE_SIZE)), int(player_y) + SCREEN_HEIGHT - (MAP_SIZE * TILE_SIZE)),
+        int(player_x) + int(SCREEN_WIDTH - (MAP_SIZE * TILE_SIZE)), int(player_y) + SCREEN_HEIGHT - int(MAP_SIZE * TILE_SIZE)),
                            6)  # PLAYER
 
 
 def cast_rays():
+
     start_angle = player_angle - HALF_FOV
 
     rectList = []
@@ -114,9 +157,6 @@ def cast_rays():
 
             cube = row * MAP_SIZE + col
 
-            ################if cube > MAP_SIZE**2 : cube -= MAP_SIZE**2
-
-
 
 
 
@@ -127,7 +167,7 @@ def cast_rays():
                 depth = (dephh - 1) * uncalc
 
 
-                precision = int((qualityBrut / ((dephh+0.001) * 1.6)))
+                precision = int((qualityBrut / ((dephh+1) * 1.6)))
                 if precision < 1: precision = 1
 
 
@@ -171,10 +211,8 @@ def cast_rays():
                 first = (SCREEN_HEIGHT + ray * SCALE - (SCREEN_WIDTH / 2) - 120)
                 second = ((SCREEN_HEIGHT / 2) - wall_height / 2,SCALE, wall_height)
 
-                rect = pygame.draw.rect(win, (color, color, color), (SCREEN_HEIGHT + ray * SCALE - (SCREEN_WIDTH / 2) + screenPatch, (SCREEN_HEIGHT / 2) - wall_height / 2, SCALE, wall_height),)
+                pygame.draw.rect(win, (color, color, color), (SCREEN_HEIGHT + ray * SCALE - (SCREEN_WIDTH / 2) + screenPatch, (SCREEN_HEIGHT / 2) - wall_height / 2, SCALE, wall_height),)
 
-                rectList.append([col, row, (SCREEN_HEIGHT + ray * SCALE - (SCREEN_WIDTH / 2) - 120, (SCREEN_HEIGHT / 2) - wall_height / 2, SCREEN_HEIGHT + ray * SCALE - (SCREEN_WIDTH / 2) - 120+ SCALE, (SCREEN_HEIGHT / 2) - wall_height / 2+ wall_height), rect])
-                # col, row, ( x1, y1, x2, y2 ), rect
 
                 # DEBUG
                 if debug == True:
@@ -205,18 +243,6 @@ while True:
             pygame.quit()
             sys.exit(0)
 
-    col = int(player_x / TILE_SIZE)
-    row = int(player_y / TILE_SIZE)
-
-    square = row * MAP_SIZE + col
-    (player_y / TILE_SIZE) * MAP_SIZE + player_x / TILE_SIZE
-    if MAP[square] == '#':
-        if forward == True:
-            player_x -= -math.sin(player_angle) * 5
-            player_y -= math.cos(player_angle) * 5
-        else:
-            player_x += -math.sin(player_angle) * 5
-            player_y += math.cos(player_angle) * 5
 
     pygame.draw.rect(win, (0, 0, 0), (0, 0, SCREEN_HEIGHT, SCREEN_HEIGHT))
 
@@ -243,16 +269,47 @@ while True:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]: player_angle -= 0.03
-    if keys[pygame.K_RIGHT]: player_angle += 0.03
+    col = int(player_x / TILE_SIZE)
+    row = int(player_y / TILE_SIZE)
+
+    square = row * MAP_SIZE + col
+    (player_y / TILE_SIZE) * MAP_SIZE + player_x / TILE_SIZE
+
+    if keys[pygame.K_LEFT]:
+        if not forward:
+            MAP = move(3, MAP)
+            forward = True
+    if keys[pygame.K_RIGHT]:
+        if not forward:
+            MAP = move(1, MAP)
+            forward = True
     if keys[pygame.K_UP]:
-        forward = True
-        player_x += -math.sin(player_angle)
-        player_y += math.cos(player_angle)
+        if not forward:
+            MAP = move(0, MAP)
+            forward = True
     if keys[pygame.K_DOWN]:
-        forward = False
-        player_x -= -math.sin(player_angle)
-        player_y -= math.cos(player_angle)
+        if not forward:
+            MAP = move(2, MAP)
+            forward = True
+
+
+
+    xTarget = (MAP.index("+")%30+0.5)*25
+    yTarget = (MAP.index("+")//30+0.5)*25
+
+    if player_x+divideSpeedMax < xTarget : player_x+=divideSpeedMax
+    elif player_x-divideSpeedMax > xTarget : player_x -= divideSpeedMax
+    elif player_y+divideSpeedMax < yTarget : player_y += divideSpeedMax
+    elif player_y-divideSpeedMax > yTarget : player_y -= divideSpeedMax
+
+
+
+    if not keys[pygame.K_DOWN] and not keys[pygame.K_LEFT] and not keys[pygame.K_UP] and not keys[pygame.K_RIGHT]: forward = False
+
+
+
+
+
 
 
     if player_x > MAP_SIZE*TILE_SIZE : player_x = 5
@@ -260,13 +317,4 @@ while True:
 
 
 
-
-    clock.tick(60)
-
-    fps = "Fps : " + str(int(clock.get_fps()))
-    font = pygame.font.SysFont('Monospace Regular', 30)
-    textsurface = font.render(fps, False, (255, 0, 00))
-    win.blit(textsurface, (0, 0))
     pygame.display.flip()
-
-
